456bf89084cb670bd363a0d1951511ba6faff32f
KiranKhanna721 <76143695+KiranKhanna721@users.noreply.github.com>
2021-10-17 22:39:53 +0530

    Update

--- main_app.py
+++ main_app.py
@@ -4,13 +4,13 @@ import streamlit as st
 import app5
 import app2
 import app3
-import app4
+#import app4
 import app1
 PAGES = {
     "Garbage": app5 ,
     "Mental Health": app2 ,
     "Health" : app3 , 
-    "Covid19": app4 ,
+ #   "Covid19": app4 ,
     "Plant_Disease" :app
 }
 st.sidebar.title('Arogya ')
